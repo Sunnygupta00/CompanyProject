3636241ff1dd4704eeec7d3c7e99d9d54d1fe1c5
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-08-21 16:00:20 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React from "react";
+import cat from "../component/cat.png";
 const Header = () => {
-    return (
-        <>
-        <div className='first'>
-         {/* <img src = {logo} /> */}
-        </div>
-        <div className='second'>
-         <button>About</button>
-         <button>Blog</button>
-         <button>Contact</button>
-         <button>career</button> 
-        </div>
-        </>
-       )
-}
+  const btn = {
+    cursor: "pointer",
+    border: "1px solid transparent",
+    backgroundColor: "transparent",
+    height: "50px",
+    width: "200px",
+    color: "black",
+    fontSize: "1em"
+  };
+  const mystyle = {
+    color: "white",
+    backgroundColor: "transparent",
+    padding: "10px",
+    fontFamily: "Arial",
+    display: "flex",
+    justifyContent: "space-between"
+  };
+  return (
+    <div style={mystyle}>
+      <div className="first">
+        <img src={cat} />
+      </div>
+      <div className="second">
+        <button style={btn}>About</button>
+        <button style={btn}>Blog</button>
+        <button style={btn}>Contact</button>
+        <button style={btn}>career</button>
+      </div>
+    </div>
+  );
+};
 
 export default Header;

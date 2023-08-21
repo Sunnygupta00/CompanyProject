3b8269ee66e9c61a43fa0e3b6c3b22f7fd08d7fb
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-08-21 15:59:55 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,7 @@
-import React, { useEffect, useState } from "react";
-const RightMidPart = () => {
-  const [flag, setFlag] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setFlag((a) => !a);
-    }, 3000);
-  });
+import React from "react";
 
-  return (
-    <div className={` ${flag ? "grey" : "blue"}`}>
-      <div>Type Text</div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, illum.
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            setFlag((a) => !a);
-          }}
-          style={{ borderRadius: "100" }}
-        >
-          click here
-        </button>
-      </div>
-    </div>
-  );
+const Footer = () => {
+  return <div>Footer</div>;
 };
 
-export default RightMidPart;
+export default Footer;

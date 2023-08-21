f7197dc861a8928a2d60ad67c7baa62bc7482942
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-08-21 16:00:57 +0530

    Update RightMidPart.jsx

--- src/components/RightMidPart.jsx
+++ src/components/RightMidPart.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
 const RightMidPart = () => {
+  const [flag, setFlag] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setFlag((a) => !a);
+    }, 3000);
+  });
+
   return (
-    <>
-    <div>Type Text</div>
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, illum.</div>
-  </>
-    )
-}
+    <div className={` ${flag ? "grey" : "blue"}`}>
+      <div>Type Text</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, illum.
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setFlag((a) => !a);
+          }}
+          style={{ borderRadius: "100" }}
+        >
+          click here
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default RightMidPart;

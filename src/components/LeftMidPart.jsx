3e92ef12ee6abef7403d1a87bb005fa399e33359
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-08-21 16:00:38 +0530

    Update LeftMidPart.jsx

--- src/components/LeftMidPart.jsx
+++ src/components/LeftMidPart.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const LeftMidPart = () => {
+  const leftStyle = {
+    fontSize: "100px;"
+  };
   return (
-   <>
-    <div className='first-container'>
+    <>
+      <div className="first-container" style={leftStyle}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, eum.
-    </div>
-    <div className='second-container'>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, eum.
-</div>
-</>
-  )
-}
+      </div>
+      <div className="second-container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, eum.
+      </div>
+    </>
+  );
+};
 
-export default LeftMidPart
+export default LeftMidPart;

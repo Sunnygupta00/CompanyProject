d12dd3e0e3a7840cc7801d242d67753eb1845d8b
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2023-08-21 12:15:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./Header";
+function App() {
+  return (
+   <div className="main-page">
+    hii
+    
+    <Header></Header>
+   </div>
+  );
+}
+
+export default App;
